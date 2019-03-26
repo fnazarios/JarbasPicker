f58bdaf9329b18650a616c247b0344b801056d88
Fernando Sousa <fnazarios@gmail.com>
2019-03-26 13:55:13 -0300

    carthage compatibility

---
+++ JarbasPicker/JarbasPicker.h
@@ -0,0 +1,19 @@
+//
+//  JarbasPicker.h
+//  JarbasPicker
+//
+//  Created by Fernando Sousa on 26/03/19.
+//  Copyright © 2019 Supreme Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JarbasPicker.
+FOUNDATION_EXPORT double JarbasPickerVersionNumber;
+
+//! Project version string for JarbasPicker.
+FOUNDATION_EXPORT const unsigned char JarbasPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JarbasPicker/PublicHeader.h>
+
+
